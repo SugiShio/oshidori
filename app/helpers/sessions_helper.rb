e9227f06222f1ce41e9f0b0a354991556d111757
杉藤菖子 <fruitmachine0@gmail.com>
2018-08-26 16:24:41 +0900

    ログインする

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,13 @@
+module SessionsHelper
+  def signin(user)
+    session[:user_id] = user.id
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def is_signin?
+    !session[:user_id].nil?
+  end
+end
---
+++ test/integration/users_login_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class UsersLoginTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
+
+  test "login with invalid information" do
+    get signin_path
+    assert_template 'sessions/new'
+    post signin_path, params: { session: { email: "", password: "" } }
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+  end
+
+  test "login with valid information" do
+    get signin_path
+    post signin_path, params: { session: { email: @user.email, password: 'password' } }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+  end
+end
